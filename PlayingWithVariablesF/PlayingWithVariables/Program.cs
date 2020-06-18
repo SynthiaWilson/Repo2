558fdd60fe8333780d4feeded61a8c6e36e4f958
Copacetic Cindy <therivermont@gmail.com>
2020-06-18 14:43:39 -0500

    Playing with Variables

---
+++ PlayingWithVariablesF/PlayingWithVariables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PlayingWithVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Ralph";
+            int dogAge = 10;
+            char dogType = '*';
+            bool dogIsAlive = true;
+
+            float dogCost = 33.3f;
+
+            double dogDouble = 4.321234555555;
+
+            decimal dogHairPercentage = 99.5m;
+
+
+            Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old,\n He is a {dogType}\n My dog is alive {dogIsAlive} \n My dog costs ${dogCost} \n My dog's hertz is {dogDouble} \n My dog has this percentage of hair % {dogHairPercentage}");
+        }
+    }
+}
